770f69bbaef880b1d27794a48a430e86fa94a9c4
adriankUSA <149288264+adriankUSA@users.noreply.github.com>
2023-11-13 23:35:05 -0500

    Step 2 Modified to read input from file
    Modified to read input from file  (DOES NOT WORK YET)

--- Project_Adrian_Khlim.java
+++ Project_Adrian_Khlim.java
@@ -1,67 +1,79 @@
-import java.util.Scanner; //allow user input
+import java.util.*; 
+import java.io.*; 
 
 public class Project_Adrian_Khlim
 {
    public static void main(String[] args)
-   {
-      Scanner keyboard = new Scanner(System.in);
-      int policyNumber, age; 
-      double heightInches, weightPounds;
-      String providerName, firstName, lastName, smokingStatus;
-      boolean isSmoker; 
-      
-      //asks user for Policy information, allows user input
-      System.out.print("Please enter the Policy Number: ");
-      policyNumber = keyboard.nextInt();
-      keyboard.nextLine();
-      System.out.print("Please enter the Provider Name: ");
-      providerName = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's First Name: ");
-      firstName = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      lastName = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's age: ");
-      age = keyboard.nextInt();
-      keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Smoking Status(smoker/non-smoker): ");
-      smokingStatus = keyboard.nextLine();
-      
-      //while smokingStatus does not equal to smoker/non-smoker, input is invalid and must repeat.
-      while(!(smokingStatus.equalsIgnoreCase("smoker"))&&!(smokingStatus.equalsIgnoreCase("non-smoker")))
-      {
-         System.out.println("Invalid error. Please type smoker or non-smoker.");
-         System.out.print("Please enter the Policyholder's Smoking Status(smoker/non-smoker): ");
-         smokingStatus = keyboard.nextLine();         
-      }
-      
-      //when smokingStatus equals smoker is true, isSmoker is true.
-      isSmoker = smokingStatus.equalsIgnoreCase("smoker");
-      
-      //continue with policy information and user input
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      heightInches = keyboard.nextDouble();
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      weightPounds = keyboard.nextDouble();
-      
-      //create a policy object titled policyInfo
-      Policy policyInfo = new Policy(policyNumber, providerName, firstName, lastName, age, isSmoker, heightInches, weightPounds);
+   {  
      
-     //display policy information
-     System.out.println(" ");
-     System.out.println("Policy Number: " + policyNumber);
-     System.out.println("Provider Name: " + providerName);
-     System.out.println("Policyholder's First Name: " + firstName);
-     System.out.println("Policyholder's Last Name: " + lastName);
-     System.out.println("Policyholder's Age: " + age);
-     System.out.println("Policyholder's Smoking Status: " + smokingStatus);
-     System.out.printf("Policyholder's Height: %.1f inches", heightInches); //single decimal
-     System.out.println(" ");
-     System.out.printf("Policyholder's Weight: %.1f pounds", weightPounds);
-     System.out.println(" ");
-     System.out.printf("Policyholder's BMI: %.2f", policyInfo.calculateBMI()); //called from the policyInfo object
-     System.out.println(" ");
-     System.out.printf("Policy Price: $%.2f", policyInfo.calculatePolicyPrice()); //called from the policyInfo object
-     System.out.println(" ");
+      try
+      {
+                 
+         //create an instance of the File class creating an object that represents the file PolicyInformation.txt
+         File file = new File("PolicyInformation.txt");
+           
+         Scanner inputFile = new Scanner(file);
+         
+         //declare variables
+         int policyNumber, age; 
+         double heightInches, weightPounds;
+         String providerName, firstName, lastName, smokingStatus;
+         boolean isSmoker;   
+         
+         //create array list to store the Policy object
+         ArrayList<Policy> policy = new ArrayList<Policy>();
+         
+         while(inputFile.hasNext())
+         {
+            policyNumber = inputFile.nextInt();
+            providerName = inputFile.nextLine();
+            firstName = inputFile.nextLine();
+            lastName = inputFile.nextLine(); 
+            age = inputFile.nextInt();
+            isSmoker = inputFile.nextBoolean();
+            heightInches = inputFile.nextDouble();
+            weightPounds = inputFile.nextDouble();
+            
+            if(inputFile.hasNext())
+            { 
+               inputFile.nextLine();
+               inputFile.nextLine();
+            }
+            
+            //create a policy object titled policyInfo
+            Policy policyInfo = new Policy(policyNumber, providerName, firstName, lastName, age, isSmoker, heightInches, weightPounds);
+            
+            policy.add(policyInfo);
+         }
+         
+         inputFile.close();
+         
+         for(int i = 0; i < policy.size(); i++)
+         {
+            //display policy information
+            System.out.println(" ");
+            System.out.println("Policy Number: " + policy.get(i).getPolicyNumber());
+            System.out.println("Provider Name: " + policy.get(i).getProviderName());
+            System.out.println("Policyholder's First Name: " + policy.get(i).getFirstName());
+            System.out.println("Policyholder's Last Name: " + policy.get(i).getLastName());
+            System.out.println("Policyholder's Age: " + policy.get(i).getAge());
+            System.out.println("Policyholder's Smoking Status: ");
+            System.out.printf("Policyholder's Height: %.1f inches" + policy.get(i).getHeight()); //single decimal
+            System.out.println(" ");
+            System.out.printf("Policyholder's Weight: %.1f pounds" + policy.get(i).getWeight());
+            System.out.println(" ");
+            System.out.printf("Policyholder's BMI: %.2f", policy.get(i).calculateBMI()); //called from the policyInfo object
+            System.out.println(" ");
+            System.out.printf("Policy Price: $%.2f", policy.get(i).calculatePolicyPrice()); //called from the policyInfo object
+            System.out.println(" ");
+         }
+      } //try block
+      
+      catch(IOException ex)      {
+         //use the getMessage method of the exception we "caught" to print out it's message about what went wrong
+         System.out.println("Something went wrong reading the file: " + ex.getMessage());
+      }
+
    }
    
 
